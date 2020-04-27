400afd7ac4a0ec41c347ea943f272861090e770c
raflifirmansyah <rafifirman198@gmail.com>
2020-04-27 14:12:57 +0800

    PerulanganWhile

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        int isiGelas = 0;
+        int penuh = 100;
+
+        System.out.println("Isi Gelas Sekarang :" + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh :" + penuh + "ml");
+
+        while (isiGelas <= penuh){
+            isiGelas++;
+            System.out.println("Sedang Mengisi Gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }
+        System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+}
