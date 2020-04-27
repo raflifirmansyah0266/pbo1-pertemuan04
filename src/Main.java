796b961d35e5e788595a79540a353cd9dee473b9
raflifirmansyah <rafifirman198@gmail.com>
2020-04-27 14:17:57 +0800

    PerulangandoWhile

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,12 @@ public class Main {
         System.out.println("Isi Gelas Sekarang :" + isiGelas + "ml");
         System.out.println("Isi Gelas Ketika Penuh :" + penuh + "ml");
 
-        while (isiGelas <= penuh){
+        do {
             isiGelas++;
             System.out.println("Sedang Mengisi Gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-        }
+        }while (isiGelas != penuh);
+
         System.out.println("Finale : Isi Gelas Sekarang : " + isiGelas + "ml");
     }
 }
